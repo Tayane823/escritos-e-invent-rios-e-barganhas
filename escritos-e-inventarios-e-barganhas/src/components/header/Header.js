9eefcddb9e939d4b00f4364cd891704a11361040
Figueiredo <tayanealves823@gmail.com>
2024-04-11 08:21:21 -0300

    start to work whit header

--- escritos-e-inventarios-e-barganhas/src/components/header/Header.js
+++ escritos-e-inventarios-e-barganhas/src/components/header/Header.js
@@ -6,7 +6,14 @@ import React from 'react';
 const Header = () => {
   return (
     <div className='div-header'>
-      
+      <h1>Título</h1>
+      <ul>
+        <li>Algo</li>
+        <li>Algo</li>
+        <li>Algo</li>
+        <li>Algo</li>
+        <li>Algo</li>
+      </ul>
     </div>
   );
 };
